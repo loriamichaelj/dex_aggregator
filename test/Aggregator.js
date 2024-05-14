046384061174db69733fe99f37d94fbd6968ae29
MJ <loriamichaelj@gmail.com>
2024-05-14 14:28:38 -0700

    setting up chai test framework

--- test/Aggregator.js
+++ test/Aggregator.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-const tokens = () => {
+const aggregator = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
