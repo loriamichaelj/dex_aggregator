42976f92833327b5bbf31e4f885a5b0a11123bc4
MJ <loriamichaelj@gmail.com>
2024-05-15 09:34:34 -0700

    installs cors, dotenv, express

--- contracts/Aggregator.sol
+++ contracts/Aggregator.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-//
+// get pricing for each token pair
 
 contract Aggregator {
     string public name = "Aggregator";
+
+    constructor(address _routerA, address _routerB) {}
 }
