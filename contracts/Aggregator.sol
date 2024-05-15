19388c069ed61855dfea1607ba3ec2cdc54eaf9a
MJ <loriamichaelj@gmail.com>
2024-05-15 13:32:36 -0700

    comments objectives for aggregator smart contract

--- contracts/Aggregator.sol
+++ contracts/Aggregator.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-// get pricing for each token pair
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+
+// [] Connect DEXes
+// [] Facilitate Swaps
 
 contract Aggregator {
     string public name = "Aggregator";
