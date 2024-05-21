5c60da169ff103e6ebd2301cd7eee15855252e79
MJ <loriamichaelj@gmail.com>
2024-05-20 17:20:55 -0700

    testing routing connection to uniswap

--- test/Aggregator.js
+++ test/Aggregator.js
@@ -3,11 +3,29 @@ const { ethers } = require("hardhat");
 const config = require("../config.json");
 
 describe("Aggregator", () => {
+  let owner;
+  let aggregator;
+
+  beforeEach(async () => {
+    [owner] = await ethers.getSigners();
+
+    const Aggregator = await ethers.getContractFactory("Aggregator");
+
+    aggregator = await Aggregator.deploy(
+      config.UNISWAP.V2_ROUTER_02_ADDRESS,
+      config.SUSHISWAP.V2_ROUTER_02_ADDRESS
+    );
+    await aggregator.deployed();
+  });
+
   describe("Deployment", () => {
-    it("has correct name", async () => {
-      const Aggregator = await ethers.getContractFactory("Aggregator");
-      let aggregator = await Aggregator.deploy();
-      expect(await aggregator.name()).to.eq("Aggregator");
+    it("Sets the owner", async () => {
+      expect(await aggregator.owner()).to.equal(await owner.getAddress());
+    });
+    it("Sets router A", async () => {
+      expect(await aggregator.routerA()).to.equal(
+        config.UNISWAP.V2_ROUTER_02_ADDRESS
+      );
     });
   });
 });
