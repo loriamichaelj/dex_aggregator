c92dc14958129b3257c21499c9c8c86c238cbb18
MJ <loriamichaelj@gmail.com>
2024-05-28 21:16:50 -0700

    configures deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,17 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 const config = require("../config.json");
 
 async function main() {
   // Deploy Aggregator
-  const aggregator = await hre.ethers.getContractFactory("Aggregator", [
+  const Aggregator = await hre.ethers.getContractFactory("Aggregator");
+  const aggregator = await Aggregator.deploy(
     config.UNISWAP.V2_ROUTER_02_ADDRESS,
-    config.SUSHISWAP.V2_ROUTER_02_ADDRESS,
-  ]);
-  await aggregator.waitForDeployment();
-  console.log(
-    `Aggregator contract deployed to: ${await aggregator.getAddress()}\n`
+    config.SUSHISWAP.V2_ROUTER_02_ADDRESS
   );
+  await aggregator.deployed();
+  console.log(`Aggregator contract deployed to: ${aggregator.address}\n`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
